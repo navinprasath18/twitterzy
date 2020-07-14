99695ac1925c4980196988b34260de55195542c8
Navin <navin.prasath@sap.com>
2020-07-14 14:32:30 +0530

    Boot App

---
+++ src/main/java/com/twitterzy/backend/TwitterzyApplication.java
@@ -0,0 +1,13 @@
+package com.twitterzy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitterzy/backend/TwitterzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitterzy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
