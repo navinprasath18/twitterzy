0e02c26d22447e602b61da40cbde573ff785d316
Navin <navin.prasath@sap.com>
2020-07-14 17:34:35 +0530

    Controller,Serice,Lambok

---
+++ src/main/java/com/twitterzy/backend/controllers/FollowManagementController.java
@@ -0,0 +1,8 @@
+package com.twitterzy.backend.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FollowManagementController {
+
+}
---
+++ src/main/java/com/twitterzy/backend/services/HTTPRequestService.java
@@ -0,0 +1,8 @@
+package com.twitterzy.backend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HTTPRequestService {
+
+}
